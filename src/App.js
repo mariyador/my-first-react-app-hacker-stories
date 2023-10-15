be93cb3aa58644e909befa4cf227d3c09c49d873
Mariya <mariya@MacBook-Air-Mariya.local>
2023-10-15 11:23:35 -0400

    3 version of funtion LIST. Drinking age

--- src/App.js
+++ src/App.js
@@ -29,8 +29,46 @@ function App() {
     </div>
   );
 }
-
-function List() {
+// // Version1
+// function List() {
+//   return(
+//     <ul>
+//       {list.map(function(item) {
+//         return (
+//           <li key={item.objectID}>
+//             <span>
+//               <a href={item.url}>{item.title}</a>
+//             </span>
+//             <span>{item.author}</span>
+//             <span>{item.num_comments}</span>
+//             <span>{item.points}</span>
+//           </li>
+//         );
+//       })}  
+//     </ul>
+//   )
+// }
+// //Version2
+// let List = function() {
+//   return(
+//     <ul>
+//       {list.map(function(item) {
+//         return (
+//           <li key={item.objectID}>
+//             <span>
+//               <a href={item.url}>{item.title}</a>
+//             </span>
+//             <span>{item.author}</span>
+//             <span>{item.num_comments}</span>
+//             <span>{item.points}</span>
+//           </li>
+//         );
+//       })}  
+//     </ul>
+//   )
+// }
+//Version3
+let List = () => {
   return(
     <ul>
       {list.map(function(item) {
