796acaac53706ba3d089f8b13888fbc17a55d538
Mariya <mariya@MacBook-Air-Mariya.local>
2023-10-15 11:05:48 -0400

    3 ways to declare function

---
+++ src/math.js
@@ -0,0 +1,30 @@
+// function square(number) {
+//     return number * number;
+// }
+
+
+// let square = function(number){
+//     return number * number
+// }
+
+let square = (number) => {
+    return number * number
+}
+
+console.log(square(5));
+
+
+// function add(a, b) {
+//     return a + b;
+// }
+
+// let add = function(a, b) {
+//     return a + b
+// }
+
+let add = (a, b) => {
+    return a + b
+}
+
+
+console.log(add(7, 6))
